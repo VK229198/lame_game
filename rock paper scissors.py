091003b7eef4f3f337fa92d688a6fc357e77d0fc
Vishvesh K Shrivatsav <145451170+VK229198@users.noreply.github.com>
2023-11-21 11:30:43 +0530

    Add files via upload
    rock paper scissors

---
+++ rock paper scissors.py
@@ -0,0 +1,34 @@
+import random as r
+l=["rock","paper","scissors"]
+cc=l[r.randint(0,2)]
+s=0
+p_a="yes"
+while p_a=="yes":
+    cc=l[r.randint(0,2)]
+    print("Your choice")
+    pc=input("rock paper scissors:").lower()
+    print("computer's choice:",cc)
+    if cc==pc:
+        print("tie")
+    elif cc=="scissors":
+        if pc=="paper":
+            print("you win")
+            s+=1
+        elif pc=="rock":
+            print("you loose")
+    elif cc=="paper":
+        if pc=="rock":
+            print("you loose")
+        elif pc=="paper":
+            print("you win")
+            s+=1
+    elif cc=="rock":
+        if pc=="scissors":
+            print("you win")
+            s+=1
+        elif pc=="paper":
+            print("you loose")
+    p_a=input("do you want to play again?:")
+print("your score is:",s)
+
+
